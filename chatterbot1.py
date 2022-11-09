b520f38f3e789d0143449528acc9ef146ca4f7c1
aciesielczyk <aciesielczyk@novelaneuro.com>
2022-11-09 11:47:21 +0100

    read only turned on

--- chatterbot1.py
+++ chatterbot1.py
@@ -1,10 +1,16 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import pytz
 
 print('Cheers')
 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.CRITICAL)
+
 bot = ChatBot(
     'Terminal',
+    read_only=True,
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
@@ -12,7 +18,7 @@ bot = ChatBot(
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': "I didn't understand your question, nevertheless check our job offers! "
                                 "https://r.mtdv.me/articles/job_offer",
-            'maximum_similarity_threshold': 0.5
+            'maximum_similarity_threshold': 0.8
         }
     ],
     database_uri='sqlite:///database.db'
